96ab6c453b9a3c34b1e65949943fb8fd7564b3d1
Programador Web 2019.2 <16124512019.2@senacrjedu.local>
2019-12-17 16:59:23 -0300

    Versão G 2.6.4

--- www/View/footer-adm.php
+++ www/View/footer-adm.php
@@ -6,7 +6,8 @@
 </div>
 <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<footer class="section footer-classic navbar navbar-expand-lg navbar-light bg-light bg-image" style="background: #2d3246;">
+
+<footer class="rodape section footer-classic navbar navbar-expand-lg navbar-light bg-light bg-image">
         <div class="container">
           <div class="row row-30">
             <div class="col-md-4 col-xl-5">
@@ -27,7 +28,7 @@
                 <dd><a href="mailto:#" class="bg-yellow">eliakin.caio.gabriel@gmail.com</a></dd>
               </dl>
               <dl class="contact-list">
-                <dt>phones:</dt>
+                <dt>Telefones: </dt>
                 <dd><a href="tel:#" class="bg-yellow">https://karosearch.com</a> <span>or</span> <a href="tel:#" class="bg-yellow">https://karosearch.com</a>
                 </dd>
               </dl>
@@ -43,16 +44,17 @@
               </ul>
             </div>
           </div>
-        </div>
+          </div> 
         <div class="social-container">
-          <div class="col"><a class="social-inner" href="#"><span><i class="fab fa-facebook-square"></i></span></a></div>
+          <div class="col"><a class="social-inner" href="#"><span><i class="fab fa-facebook-square fa-2x"></i></span></a></div>
           
-          <div class="col"><a class="social-inner bg-yellow" href="#"><span><i class="fab fa-instagram"></i></span></a></div>
-          <div class="col"><a class="social-inner bg-yellow" href="#"><span><i class="fab fa-twitter-square"></i></span></a></div>
-          <div class="col"><a class="social-inner bg-yellow" href="#"><span><i class="fab fa-google-plus-square"></i></span></a></div>
+          <div class="col"><a class="social-inner bg-yellow" href="#"><span><i class="fab fa-instagram fa-2x"></i></span></a></div>
+          <div class="col"><a class="social-inner bg-yellow" href="#"><span><i class="fab fa-twitter-square fa-2x"></i></span></a></div>
+          <div class="col"><a class="social-inner bg-yellow" href="#"><span><i class="fab fa-google-plus-square fa-2x"></i></span></a></div>
         
         </div>
-    
+        
+         
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     
@@ -66,5 +68,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </footer>
+    
   </body>
 </html>
