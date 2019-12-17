e7bd2108f5dbc74e626fa4008ceb053051442032
Programador Web 2019.2 <16124512019.2@senacrjedu.local>
2019-12-17 14:07:12 -0300

    Versão G 2.6.0

--- www/incluedes/Seguranca.php
+++ www/incluedes/Seguranca.php
@@ -6,6 +6,7 @@ class Seguranca{
     public static function restritoAdm(){
         if(!isset($_SESSION['usuarioid'])){
             header("location: http://www.qualificaroficina.web70113.uni5.net/login/adm");
+           // header("location: http://localhost/Oficial2/www/login/adm");
         }
     }
 }
