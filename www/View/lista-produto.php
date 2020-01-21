a030b0af5a8fc84671f289ff3d5f7b3b34aa00d5
Tecnico em Informatica 2019.2 <16121742019.2@senacrjedu.local>
2020-01-21 14:06:58 -0300

    Últimas Semana

--- www/View/lista-produto.php
+++ www/View/lista-produto.php
@@ -1,16 +1,53 @@
 
 <?php include "View/header.php";?>
+<h1 class="no-centro">Qualificar Produtos</h1>
+<hr>
+</div>
 <aside>
+
 <div>
-    <ul>
-        <li>Cateho</li>
-        <li>oi</li>
-        <li>oi</li>
-        <li>oi</li>
-    </ul>
+<div class="navbar-light  vertical-menu oll ">
+  <h4  class="cont"><i class="farra fas fa-car"></i> <strong class="mr-auto">Categorias </strong></h4>
+  <a class="no-centro" href="#"> Pneus </a>
+  <a class="no-centro" href="#"> Aros </a>
+  <a class="no-centro" href="#"> Óleo </a>
+  <a class="no-centro" href="#"> Peças </a>
+
+  <hr>
+</div>
+</div>
+<div class="container">
+<div class="row " id="ads">
+    <?php foreach($lista as $produto) { ?>
+    
+    <!-- Category Card -->
+    <div class="col-md-4 top ">
+        <div class="card rounded ">
+            <div class="card-image box21">
+                <span class="card-notify-badge">Ótimo Rendimento</span>
+                <span class="card-notify-year">2020</span>
+                <img class="img-fluid" src="<?php echo $url;['imagem'] ?>/View/img/produtos/<?php echo $produto['imagem'] ?>" 
+                alt="Alternate Text" />
+            </div>
+            <div class="card-image-overlay m-auto ">
+                <span class="card-detail-badge">Gratis: <?php  echo $produto['servico']?></span>
+                <span class="card-detail-badge">R$:<?php echo number_format($produto['preco'],2,",",".");?></span>
+                
+            </div>
+            <div class="card-body text-center">
+                <div class="ad-title m-auto">
+                    <h5><?php echo $produto['nome']?></h5> <a class="detalhe" href="/Oficial2/www/detalhes/produto" ><i class="fas fa-info-circle fa-2x "></i></a>
+                </div>
+                <a class="ad-btn" href="<?php echo $url;?>/carrinho/adicionar/<?php echo $produto ['id']?>">Comprar</a>
+            </div>
+        </div>
+    </div>
+ 
+    <?php } ?>
+    </div>
 </aside>
 <section>
-<table class="table">
+<!--<table class="table">
     <thead>
         <tr>
             <th scope="col"></th>
@@ -41,5 +78,5 @@
         <?php } ?>
     </tbody>
 </table>
-</section>
+</section>-->
 <?php include "View/footer.php";?>
